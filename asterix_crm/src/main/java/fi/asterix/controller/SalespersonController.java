76ce14eb4b71f48906e6a584e21feba26b5e51f1
marika.java <marika.naivo@suomi24.fi>
2015-11-19 23:12:58 +0200

    muokattu Customerin lomakemetodeita

--- asterix_crm/src/main/java/fi/asterix/controller/SalespersonController.java
+++ asterix_crm/src/main/java/fi/asterix/controller/SalespersonController.java
@@ -1,6 +1,8 @@
 package fi.asterix.controller;
 
 import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.validation.Valid;
 
@@ -18,18 +20,21 @@ import fi.asterix.DAO.CompanyDAO;
 import fi.asterix.DAO.DealDAO;
 import fi.asterix.DAO.CustomerDAO;
 import fi.asterix.JPA.Address;
+import fi.asterix.JPA.Company;
 import fi.asterix.JPA.Customer;
+import fi.asterix.JPA.Employee;
+import fi.asterix.JPA.State;
 
 @Controller
-@RequestMapping(value="/secure/salesperson")
+@RequestMapping(value = "/secure/salesperson")
 public class SalespersonController {
 
 	@Inject
 	private CompanyDAO companyDAO;
-	
+
 	@Inject
 	private DealDAO dealDAO;
-	
+
 	@Inject
 	private CustomerDAO custoDAO;
 
@@ -43,22 +48,22 @@ public class SalespersonController {
 	public String addCompany(Model model) {
 		return "secure/salesperson/addcompany";
 	}
-	
+
 	@RequestMapping(value = "/savecompany", method = RequestMethod.POST)
 	public String saveCompany(Model model) {
 		return "secure/salesperson/savecompany";
 	}
-	
+
 	@RequestMapping(value = "/addnewdeal", method = RequestMethod.GET)
 	public String addDeal(Model model) {
 		return "secure/salesperson/addnewdeal";
-	}	
-	
+	}
+
 	@RequestMapping(value = "/savedeal", method = RequestMethod.POST)
 	public String saveDeal(Model model) {
 		return "secure/salesperson/savedeal";
 	}
-	
+
 	// Listataan kaikki asiakkaat
 	@RequestMapping(value = "/customermainpage", method = RequestMethod.GET)
 	public String findAllCustomers(Model model) {
@@ -73,8 +78,24 @@ public class SalespersonController {
 	// Lomakkeen luominen
 	@RequestMapping(value = "/newcustomer", method = RequestMethod.GET)
 	public String newForm(Model model) {
+
 		Customer tk = new Customer();
+
 		model.addAttribute("customer", tk);
+		
+		// Haetaan address, company, state ja vastuumyjä
+		// Huom! Vastuumyyjä voi nyt olla kuka tahansa henkilöstöstä, eli ei saisi oikeasti toimia näin!
+		Map<Integer, String> addresses = custoDAO.getAddressSelect();
+		model.addAttribute("addresses", addresses);
+
+		Map<Integer, String> companies = custoDAO.getCompanySelect();
+		model.addAttribute("companies", companies);
+
+		Map<Integer, String> customerstates = custoDAO.getStateSelect();
+		model.addAttribute("customerstates", customerstates);
+
+		Map<Integer, String> salespersons = custoDAO.getEmployeeSelect();
+		model.addAttribute("salespersons", salespersons);
 
 		return "secure/salesperson/newcustomer";
 	}
@@ -86,10 +107,13 @@ public class SalespersonController {
 		// haetaan DAO:sta lomakkeelta syötetyllä id-arvolla Address-tietue
 		Address address = custoDAO.getAddress(tk.getAddess().getId());
 		tk.setAddess(address);
-		
+
+		System.out.println("Lomakkeen tiedot (Customer) ovat: " + tk);
+
 		// tallennetaan lomakkeelta luettu tieto
 		Customer custo = custoDAO.save(tk);
 		model.addAttribute("customer", custo);
-		return "secure/salesperson/customermainpage";
+
+		return "secure/salesperson/tools";
 	}
 }
